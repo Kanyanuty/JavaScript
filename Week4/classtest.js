b3046f4206c1cab905eca5bf51a940ea8fbdc155
Kanyanuty <6206021612148@fitm.kmutnb.ac.th>
2021-01-27 08:49:07 +0700

    update

---
+++ Week4/classtest.js
@@ -0,0 +1,13 @@
+class Song{
+    constructor(){
+        this.title;
+        this.author;
+    }
+
+    plsy(){
+        console.log("Song playing!")
+    }
+}
+
+const mySong = new Song();
+mySong.plsy();
