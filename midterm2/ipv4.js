0a0e91cda3b8c35b4b769ad34ba7ed714278cb5f
Kanyanuty <6206021612148@fitm.kmutnb.ac.th>
2021-02-24 18:32:44 +0700

    midterm web

--- midterm2/ipv4.js
+++ midterm2/ipv4.js
@@ -1,5 +1,10 @@
 function isValidIP(){
-    
+
 }
 
 console.log(isValidIP("1.2.3.4"));//==>true
+console.log(isValidIP("1.2.3"));//==>false
+console.log(isValidIP("1.2.3.4.5"));//==>false
+console.log(isValidIP("123.45.67.89"));//==>true
+console.log(isValidIP("123.456.78.90"));//==>false
+console.log(isValidIP("123.045.067.089"));//==>false
--- midterm2/timecon.js
+++ midterm2/timecon.js
@@ -2,9 +2,31 @@ function digitalClock(time){
     let sec;
     let mins;
     let hour;
-    
+    sec = time%60;// to second
+    time = Math.floor(time/60);
+    mins = time%60;// to minis
+    time = Math.floor(time/60);
+    //check NextDay
+    if(time >= 24){
+        hour = time%24;
+    }
+    else{
+        hour = time;
+    }
+    //add zero to front
+    if(hour<10){
+        hour = "0"+hour;
+    }
+    if(mins<10){
+        mins = "0"+mins;
+    }
+    if(sec<10){
+        sec = "0"+sec;
+    }
+
+    return hour+":"+mins+":"+sec;//output
 }
 
-console.log(digitalClock(5025));
-console.log(digitalClock(61201));
-console.log(digitalClock(87000));
+console.log(digitalClock(5025));//=== 01.23.45s
+console.log(digitalClock(61201));//=== 17.00.01s
+console.log(digitalClock(87000));//=== 00.10.00s nextDay
