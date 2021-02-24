49aeb775547f0c6defd1625c3b7210ad3250bf39
Kanyanuty <6206021612148@fitm.kmutnb.ac.th>
2021-02-24 08:49:13 +0700

    update midturm

--- midturm/Totalvolume.js
+++ midturm/Totalvolume.js
@@ -3,9 +3,9 @@ function totalVolume(){
     for(let i = 0; i<arguments.length ; i++){
         sum+=arguments[i][0]*arguments[i][1]*arguments[i][2];
     }
+    return sum;
 }
 
 console.log(totalVolume([4,2,4],[3,3,3],[1,1,2],[2,1,1]));
 console.log(totalVolume([2,2,2],[2,1,1]));
 console.log(totalVolume([1,1,1]));
-//console.log(arguments[0]);
