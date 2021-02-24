c27da4a63f18a0d8e0bcca9c1cbea75c4e8c4022
Kanyanuty <6206021612148@fitm.kmutnb.ac.th>
2021-02-24 18:55:44 +0700

    update

--- midterm2/ipv4.js
+++ midterm2/ipv4.js
@@ -1,5 +1,22 @@
-function isValidIP(){
-
+function isValidIP(str){
+    arr = str.split(".");
+    if(arr.length != 4){
+        return false;
+    }
+    for(let  i of arr){
+        if(!(i.length == 1)){
+            if(i.charAt(0) == 0){
+                return false;
+            }
+        }
+        if(parseInt(i) > 255 ||parseInt(i) < 0){
+            return false;
+        }
+    }
+    if(parseInt(arr[arr.length-1]) == 0){
+        return false;
+    }
+    return true;
 }
 
 console.log(isValidIP("1.2.3.4"));//==>true
